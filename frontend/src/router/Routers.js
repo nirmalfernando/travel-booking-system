285ffbcee04b3e1e9f60c7ee121d3661de0c39c8
Nirmal Fernando <112607619+nirmalfernando@users.noreply.github.com>
2023-12-11 23:51:48 +0530

    Add files via upload

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Home from './../pages/Home';
+import Login from './../pages/Login';
+import Register from './../pages/Register';
+import SearchResult from './../pages/SearchResult';
+import TourDetails from './../pages/TourDetails';
+import Tours from './../pages/Tours';
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Navigate to="/home"/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/tours" element={<Tours/>}/>
+        <Route path="/tours/:id" element={<TourDetails/>}/>
+        <Route path="/tours/search" element={<SearchResult/>}/>
+    </Routes>
+  );
+};
+
+export default Routers
